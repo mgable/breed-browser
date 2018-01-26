844179889198dc314e18d264a8379a06f13bd307
Mark Gable <mgable43@gmail.com>
2018-01-26 09:21:33 -0800

    quiz css issues

--- src/About.js
+++ src/About.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
 
 class About extends React.Component{
 	render(){
 		return (
-			<div className="About">
+			<div className="about">
 				<p>Doggo ipsum borkdrive most angery pupper I have ever seen heckin much ruin diet aqua doggo, smol what a nice floof the neighborhood pupper. Smol borking doggo with a long snoot for pats adorable doggo sub woofer woofer the neighborhood pupper shoob heckin angery woofer you are doin me a concern pupper, aqua doggo heckin angery woofer woofer h*ck extremely cuuuuuute stop it fren. Long bois snoot waggy wags puggo boofers, shooberino stop it fren smol. Shoober shooberino you are doing me a frighten borking doggo smol, borkdrive smol borking doggo with a long snoot for pats pupperino.</p>
 
 				<p>Thicc tungg very taste wow heckin pats doggo very good spot very hand that feed shibe, long bois puggo doggorino length boy smol. Very good spot heckin good boys doge shoober yapper doggo, super chub many pats pats doing me a frighten. Very jealous pupper heck floofs heckin good boys shooberino puggorino, you are doing me the shock heckin good boys and girls I am bekom fat. Very hand that feed shibe super chub blop thicc maximum borkdrive long doggo, waggy wags heck shibe. H*ck shoob long bois long doggo long bois, heckin good boys and girls he made many woofs wow very biscit what a nice floof, shooberino h*ck snoot. blop tungg yapper. floofs fluffer. Puggorino I am bekom fat long doggo heck pupper shooberino heckin angery woofer, boofers big ol pupper long doggo heckin blop, tungg yapper long water shoob mlem adorable doggo. </p>
