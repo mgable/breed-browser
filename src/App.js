9a4bee5f871e6841172ac216ed2da5d3de583205
Mark Gable <mgable43@gmail.com>
2018-03-30 13:59:14 -0700

    move footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import BasicDisplay from './BasicDisplay.js';
 import Nav from './Nav.js';
-import Footer from './Footer.js';
+import Footer from './Footer/Footer.js';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
