937db420583b10fca2cf57b237b5fc3d8a19cd69
Mark Gable <mgable43@gmail.com>
2018-04-03 14:24:53 -0700

    use ref's instead of id's

--- src/BreedBrowser/components/SearchField.js
+++ src/BreedBrowser/components/SearchField.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 
+console.info(React);
+
 // using 'class' not 'const' on purpose
 class SearchField extends Component {
 	constructor(props) {
 		super(props);
+
+		this.setTextInputRef = element => {
+			this.textInput = element;
+		};
+
 		this.state = {breed: props.breed, fieldName: 'search', field: null};
 		this.clearSearch = this.clearSearch.bind(this);
 		this.handleChange = this.handleChange.bind(this);
-	}
-
-	componentDidMount(){
-		var field = document.getElementById(this.state.fieldName);
-		this.setState({field})
 	}
 
 	handleChange(event) {
@@ -21,7 +23,7 @@ class SearchField extends Component {
 	}
 
 	clearSearch(){
-		this.state.field.focus();
+		this.textInput.focus();
 		this.setState({breed: ""});
 		this.props.onFilterBreeds("");
 	}
@@ -31,7 +33,7 @@ class SearchField extends Component {
 			<div className="search-wrapper">
 				<form>
 					<label htmlFor="search">Search breeds:&nbsp;
-						<input type="text" name="focus" required className="search-box search" onChange={this.handleChange} id="search" value={this.state.breed} placeholder="search" />
+						<input ref={this.setTextInputRef} type="text" name="focus" required className="search-box search" onChange={this.handleChange} id="search" value={this.state.breed} placeholder="search" />
 						<button onClick={this.clearSearch} className="close-icon" type="reset" />
 					</label>
 				</form>
